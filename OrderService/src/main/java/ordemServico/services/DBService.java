2cb2144de747a35c1987335fa359b1a8a91d2532
Marcus Aurélio M. O. Santos <90445199+marcusaurelio743@users.noreply.github.com>
2026-07-19 11:05:39 -0300

    criando perfil

---
+++ OrderService/src/main/java/ordemServico/services/DBService.java
@@ -0,0 +1,35 @@
+package ordemServico.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ordemServico.domain.Chamado;
+import ordemServico.domain.Cliente;
+import ordemServico.domain.Tecnico;
+import ordemServico.domain.enums.Prioridade;
+import ordemServico.domain.enums.Status;
+import ordemServico.repository.ChamadoRepository;
+import ordemServico.repository.ClienteRepository;
+import ordemServico.repository.TecnicoRepository;
+
+@Service
+public class DBService {
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	public void instanciaDB() {
+		Cliente cliente = new Cliente(null, "Jose", "123", "jose@email.com", "admin");
+		Tecnico tecnico = new Tecnico(null, "Maria", "5555", "Maria@email.com", "123");
+		
+		Chamado chamado = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Reparo em pc", "desmontar e consertar placa mae", tecnico, cliente);
+		clienteRepository.save(cliente);
+		tecnicoRepository.save(tecnico);
+		chamadoRepository.save(chamado);
+		
+	}
+
+}
---
+++ OrderService/src/main/java/ordemServico/test/TestConfig.java
@@ -0,0 +1,20 @@
+package ordemServico.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import ordemServico.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instanciaDB() {
+		this.dbService.instanciaDB();
+	}
+}
